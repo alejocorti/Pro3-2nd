ebbb0bdb2c51dee5afaad974e9b27e66de9f9730
alejocorti <acortigiganti@udesa.edu.ar>
2023-11-15 16:40:15 -0300

    Arreglando errores de firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,13 @@ import firebase from 'firebase';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDqwKxiIDkFUZT4RuUjipAidtrkAkNS3z8",
-    authDomain: "prog3parte2.firebaseapp.com",
-    projectId: "prog3parte2",
-    storageBucket: "prog3parte2.appspot.com",
-    messagingSenderId: "10375262609",
-    appId: "1:10375262609:web:b7675d87fadced8e816a30"
-  };
-  
+  apiKey: "AIzaSyDqwKxiIDkFUZT4RuUjipAidtrkAkNS3z8",
+  authDomain: "prog3parte2.firebaseapp.com",
+  projectId: "prog3parte2",
+  storageBucket: "prog3parte2.appspot.com",
+  messagingSenderId: "10375262609",
+  appId: "1:10375262609:web:b7675d87fadced8e816a30"
+};
 
 app.initializeApp(firebaseConfig);
 
